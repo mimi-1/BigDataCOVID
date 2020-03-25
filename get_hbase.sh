feeca7a793c7ca666d086053d89ce53b67773b9c
Marina <maria_dev@sandbox-hdp.hortonworks.com>
2020-03-25 17:12:24 +0000

    copies file to tmp/as2 in hdfs

--- get_hbase.sh
+++ get_hbase.sh
@@ -5,8 +5,11 @@ echo -e "create 'epidemic_tracker','classification','location','patient','desc',
 
 
 # get raw detailed data  into currect directory 
-wget --continue --timestamping https://raw.githubusercontent.com/beoutbreakprepared/nCoV2019/master/latest_data/latestdata.csv
 
-#load file into hbase
+url="https://raw.githubusercontent.com/beoutbreakprepared/nCoV2019/master/latest_data/latestdata.csv"
+wget $url
 
-hdfs dfs -put latestdata.csv /tmp 
+#load file into hdfs
+
+hdfs dfs -mkdir /tmp/as2
+hdfs dfs -put latestdata.csv /tmp/as2 
